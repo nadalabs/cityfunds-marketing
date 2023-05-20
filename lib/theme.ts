1b3e075751b13d1c0f69ed5d80c9aea3a0f13159
Ethan <ethan.blumenthal@gmail.com>
2023-05-20 10:29:07 -0500

    Add styles and base layout.

---
+++ lib/theme.ts
@@ -0,0 +1,37 @@
+const theme = {
+  colors: {
+    primary: '#1A1A1A',
+    secondary: '#48DC95',
+    success: '#28a745',
+    danger: '#FF554F',
+    warning: '#ffc107',
+    info: '#17a2b8',
+    light: '#f8f9fa',
+    dark: '#303030',
+    white: '#ffffff',
+    gray: '#989898',
+  },
+  fonts: {
+    main: '"Poppins", sans-serif',
+    secondary: '"Work Sans", sans-serif',
+  },
+  fontSizes: {
+    small: '0.8rem',
+    medium: '1rem',
+    large: '1.2rem',
+  },
+  breakpoints: {
+    xs: '320px',
+    sm: '576px',
+    md: '768px',
+    lg: '992px',
+    xl: '1200px',
+  },
+  spacing: {
+    small: '8px',
+    medium: '16px',
+    large: '24px',
+  },
+};
+
+export default theme;
---
+++ src/pages/index.tsx
@@ -0,0 +1,41 @@
+import Head from 'next/head';
+import HeroSection from '../sections/HeroSection';
+import LocationSection from '../sections/LocationSection';
+import CitySection from '../sections/CitySection';
+import FeaturedSection from '../sections/FeaturedSection';
+import ValueProps from '../sections/ValueProps';
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <meta charSet="utf-8" />
+        <link rel="icon" href="/favicon.ico" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;900&display=swap"
+          rel="stylesheet"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Work+Sans:wght@400;600;900&display=swap"
+          rel="stylesheet"
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="theme-color" content="#000000" />
+        <meta
+          name="description"
+          content="Gain access to professionally-managed, diversified real estate portfolios in the world’s best cities - start with as little as $100."
+        />
+        <title>Nada</title>
+      </Head>
+
+      <HeroSection />
+
+      <div style={{ margin: '0 156px' }}>
+        <FeaturedSection />
+        <CitySection />
+        <LocationSection />
+        <ValueProps />
+      </div>
+    </>
+  );
+}
