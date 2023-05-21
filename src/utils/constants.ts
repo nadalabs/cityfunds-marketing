ee05de4a61b8b8fc3fd7fd19483651ea295eb4ed
Ethan <ethan.blumenthal@gmail.com>
2023-05-21 15:43:07 -0500

    Add images and utils folders.

---
+++ src/utils/constants.ts
@@ -0,0 +1,6 @@
+export enum EXTERNAL_ROUTES {
+  WEB_APP = 'https://www.invest.nada.co',
+  TERMS = 'https://www.nada.co/terms-conditions',
+  PRIVACY = 'https://www.nada.co/privacy-policy',
+  DOWNLOAD = 'https://nada.onelink.me/wNEn/ksbeffng',
+}
