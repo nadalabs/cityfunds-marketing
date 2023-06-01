5c88e8878e162c037a7594b2561a88e1aa396152
Ethan <ethan.blumenthal@gmail.com>
2023-06-01 12:31:47 -0500

    Add paragraph text.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,5 @@
 import { LinkButton } from '@elements/Buttons';
-import { LinkText, TertiaryHeading } from '@elements/Typography';
+import { Caption, LinkText, PrimaryText, TertiaryHeading } from '@elements/Typography';
 import { EXTERNAL_ROUTES, FOOTER_LINKS, SOCIAL_LINKS } from '@utils/constants';
 import Image from 'next/image';
 import Link from 'next/link';
@@ -76,6 +76,65 @@ export default function Footer({}) {
         title={data.post.title}
         content={data.post.content}
       /> */}
+
+      <Caption>
+        © 2023 Nada Holdings, Inc. and/or its affiliates. All rights reserved.
+        Nada is a registered service mark of Nada Holdings, Inc.
+      </Caption>
+
+      <Caption>
+        Nada Holdings, Inc. ("Nada"), as a manager of Cityfunds I, LLC
+        (“Cityfunds”) operates the www.nada.co website (the "site") and the
+        mobile-based app (the "App") and is not a broker-dealer or investment
+        advisor. All securities ("Shares") related activity is conducted through
+        Dalmore Group LLC, a registered broker-dealer and member of FINRA/SIPC,
+        located at 525 Green Place, Woodmere, NY 11598. You can review the
+        brokercheck for Dalmore.
+      </Caption>
+      
+      <Caption>
+        You should speak with your financial advisor, accountant and/or attorney
+        when evaluating any offering. Neither Nada, Cityfunds, nor Dalmore makes
+        any recommendations or provides advice about investments, and no
+        communication, through this website or in any other medium, should be
+        construed as a recommendation for any security offered on or off this
+        investment platform. The Site may make forward-looking statements. You
+        should not rely on these statements but should carefully evaluate the
+        offering materials in assessing any investment opportunity, including
+        the complete set of risk factors that are provided as part of the
+        offering circular for your consideration.
+      </Caption>
+      <Caption>
+        Cityfunds is conducting public offerings pursuant to Regulation A, as
+        amended, through the Site and App. The offering circular for Cityfunds
+        is available. Past performance is no guarantee of future results.
+        Investments such as those on the Nada platform are speculative and
+        involve substantial risks to consider before investing, outlined in the
+        respective offering materials and including, but not limited to,
+        illiquidity, lack of diversification and complete loss of capital. Key
+        risks include, but are not limited to, limited operating history,
+        limited diversification, risk of asset damage or theft and lack of
+        voting rights. Also, the adverse economic effects of the COVID-19
+        pandemic remain unknown and could materially impact this investment. An
+        investment in an offering constitutes only an investment in a particular
+        series and not in Cityfunds or the underlying asset(s). Investors should
+        carefully review the risks located in the respective offering materials
+        for a more comprehensive discussion of risk.
+      </Caption>
+      <Caption>
+        From time to time, Cityfunds will seek to qualify additional series
+        offerings of securities under Regulation A. For offerings that have not
+        yet been qualified, no money or other consideration is being solicited
+        and, if sent in response, will not be accepted. No offer to buy
+        securities of a particular offering can be accepted, and no part of the
+        purchase price can be received until an offering statement filed with
+        the Securities and Exchange Commission (the "SEC") relating to that
+        series has been qualified by the SEC. Any such offer may be withdrawn or
+        revoked, without obligation or commitment of any kind, at any time
+        before notice of acceptance given after the date of qualification by the
+        SEC. An indication of interest involves no obligation or commitment of
+        any kind.
+      </Caption>
     </SectionWrapper>
   );
 }
--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,13 +1,16 @@
 import PageLayout from '@components/PageLayout';
+import { SectionWrapper } from '@elements/Containers';
+import { Heading, Overline, PrimaryText } from '@elements/Typography';
 import CardSlider from '@sections/CardSlider';
 import CareersCTA from '@sections/CareersCTA';
 import FeaturedLogos from '@sections/FeaturedLogos';
-import LongFormText from '@sections/LongFormText';
 import PageHero from '@sections/PageHero';
 import TextSlider from '@sections/TextSlider';
 import { FEATURED_BACKERS, OUR_VALUES } from '@utils/constants';
+import { contentByLabelQuery, teammateIndexQuery } from 'lib/queries';
+import { getClient } from 'lib/sanity.server';
 
-export default function AboutPage() {
+export default function AboutPage({ data }) {
   return (
     <PageLayout>
       <PageHero
@@ -20,11 +23,44 @@ export default function AboutPage() {
           },
         ]}
       />
-      <LongFormText
-        overline="We are on a Mission"
-        title="Our Story"
-        content={[]}
-      />
+      {/* <LongFormText
+        overline={`We are on a Mission`}
+        title={`Our Story`}
+        content={``}
+      /> */}
+      <SectionWrapper>
+        <Overline>We are on a Mission</Overline>
+        <Heading>Our Story</Heading>
+        <PrimaryText>
+          Nada is an investment, finance, and banking platform that has
+          redefined how everyone accesses real estate assets.‍
+        </PrimaryText>
+        <PrimaryText>
+          Nada was founded on the belief that the financial system built around
+          real estate assets was far too restrictive and unnecessarily
+          complicated. We realized that the industry insiders and the wealthy
+          had an unfair advantage. So, we set out to level the playing field by
+          creating new financial products with transparency and simplicity built
+          into every step.‍
+        </PrimaryText>
+        <PrimaryText>
+          Founded by a couple of non-conformists with the experience and grit to
+          break down these barriers. Co-founder and CEO John Green spent his 20s
+          as a full-time touring & recording punk rocker before his career
+          leading risk and strategy within the mortgage industry. Co-founder and
+          CFO Mauricio Delgado dropped out of Stanford to start his first
+          company at 19 before launching a career spanning Wall Street as CEO of
+          an auto fintech company.
+        </PrimaryText>
+        <PrimaryText>
+          Today, Nada is powered by a diverse group of talented and
+          purpose-driven people who believe everyone deserves access to real
+          estate wealth. We have created financial products enabling everyone to
+          access home equity by investing as little as $250 in a single city or
+          spending equity on a Visa® debit card. We're not just a company; we're
+          a team of people who want to do good in the world.
+        </PrimaryText>
+      </SectionWrapper>
       <FeaturedLogos overline="World Class Backing" logos={FEATURED_BACKERS} />
       <CardSlider
         heading="Our Team"
@@ -47,11 +83,6 @@ export default function AboutPage() {
             description: 'VP Revenue',
             imageUrl: '/images/sundance.png',
           },
-          {
-            title: 'Jeremy Males',
-            description: 'VP Investments',
-            imageUrl: '/images/jeremy.png',
-          },
         ]}
       />
       <TextSlider
@@ -63,4 +94,22 @@ export default function AboutPage() {
       <CareersCTA />
     </PageLayout>
   );
+}
+
+export async function getStaticProps({ params, preview = false }) {
+  const data = await getClient(preview).fetch(contentByLabelQuery, {
+    label: 'footer',
+  });
+  const teammates = await getClient(preview).fetch(teammateIndexQuery);
+  const content = data?.content ?? null;
+  const teammate = teammates?.content ?? null;
+
+  return {
+    props: {
+      preview,
+      data: { content, teammate },
+    },
+    // If webhooks isn't setup then attempt to re-generate in 1 minute intervals
+    revalidate: process.env.SANITY_REVALIDATE_SECRET ? undefined : 60,
+  };
 }
