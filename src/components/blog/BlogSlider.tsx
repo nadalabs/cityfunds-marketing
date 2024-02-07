215b952b2f852bdd224f218eeb1acffc2060e248
Ethan <ethan.blumenthal@gmail.com>
2024-02-07 16:38:45 -0600

    Fix blog sliders.

--- src/components/blog/BlogSlider.tsx
+++ src/components/blog/BlogSlider.tsx
@@ -37,7 +37,7 @@ export default function BlogSlider({ tag, blogPosts }: BlogSliderProps) {
     dots: false,
     infinite: false,
     speed: 500,
-    slidesToShow: isMobile ? 1.15 : 4,
+    slidesToShow: isMobile ? 1.15 : blogPosts.length < 4 ? blogPosts.length : 4,
     slidesToScroll: isMobile ? 1 : 4,
     swipeToSlide: true,
     beforeChange: (_, next) => setActiveStep(next),
