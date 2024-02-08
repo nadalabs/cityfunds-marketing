0a7c24c2f9ebbc7b1d44326f886493a79a6d7480
Ethan <ethan.blumenthal@gmail.com>
2024-02-08 09:22:43 -0600

    Add UTM tracking.

--- src/app/(tabs)/layout.tsx
+++ src/app/(tabs)/layout.tsx
@@ -3,12 +3,24 @@ import GlobalStyles from '@utils/styles';
 import DesktopNavBar from '@components/common/DesktopNavBar';
 import MobileNavBar from '@components/common/MobileNavBar';
 import theme from '@utils/theme';
-import { ReactNode } from 'react';
+import { ReactNode, useEffect } from 'react';
 import { ThemeProvider } from 'styled-components';
 import { LayoutWrapper, MainWrapper } from '@elements/Containers';
 import PageFooter from '@components/common/PageFooter';
+import { UTM_PARAMETERS } from '@utils/constants';
+import { setCookie } from '@utils/helpers';
 
 export default function TabsLayout({ children }: { children: ReactNode }) {
+  useEffect(() => {
+    const urlParams = new URLSearchParams(window.location.search);
+    UTM_PARAMETERS.forEach((param) => {
+      if (urlParams.has(param)) {
+        const value = urlParams.get(param);
+        setCookie(param, value);
+      }
+    });
+  }, []);
+
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyles />
